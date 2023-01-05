5f9d2e9d394b5f0e4ea75d01b59f862d7f8b816c
Verdant31 <joao.pedro3110@hotmail.com>
2023-01-05 11:17:26 -0400

    changing label location

--- src/components/content/Print/Canvas.tsx
+++ src/components/content/Print/Canvas.tsx
@@ -15,39 +15,39 @@ export function MyCanvas() {
     fireMiddleClick: true,
     stopContextMenu: true,
   });
-  canvas.setWidth(680.31496063);
-  canvas.backgroundColor = "#afa2a2";
+  canvas.setWidth(642.51968504);
+  canvas.backgroundColor = "#fff";
   canvas.setHeight(1133.8582677);
 
-  let initialLeft = 46.488188976;
-  let initialTopName = 5;
-  let initialTopPrice = 34;
+  let initialLeft = 55;
+  let initialTopName = 34;
+  let initialTopPrice = 10;
+  console.log("opa");
   for (let i = 1; i <= 85; i++) {
     canvas.add(
       new fabric.Text("Colar branco X", {
         left: initialLeft,
         top: initialTopName,
-
-        fontSize: 16,
+        fontSize: 14,
       })
     );
     canvas.add(
       new fabric.Text("R$48,90", {
         left: initialLeft,
         top: initialTopPrice,
-        fontSize: 16,
+        fontSize: 14,
       })
     );
-    initialLeft += 22.677165354 + 94.488188976;
+    initialLeft += 15.118110236 + 97.377952756;
     if (i % 5 === 0) {
-      initialLeft = 46.488188976;
-      initialTopName += 56.692913386 + 10;
-      initialTopPrice += 56.692913386 + 10;
+      initialLeft = 55;
+      initialTopName += 56.692913386 + 7.5590551181;
+      initialTopPrice += 56.692913386 + 7.5590551181;
     }
   }
   const imgData = canvas.toDataURL("image/jpeg");
   // eslint-disable-next-line new-cap
-  const pdf = new jsPDF("p", "mm", [180, 300]);
+  const pdf = new jsPDF("p", "mm");
 
   pdf.addImage(imgData, "JPEG", 0, 0);
   pdf.save("download.pdf");
