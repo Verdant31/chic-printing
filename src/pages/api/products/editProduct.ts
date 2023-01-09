c773e419f406a5178c7ee855935fd7e071d785c4
Verdant31 <joao.pedro3110@hotmail.com>
2023-01-09 19:27:52 -0400

    fixing price format on prisma

--- src/pages/api/products/editProduct.ts
+++ src/pages/api/products/editProduct.ts
@@ -10,7 +10,7 @@ export default async function handler(
     const updatedProduct = await prisma.product.update({
       data: {
         name,
-        price: parseFloat(price.replace(",", ".")),
+        price: price.replace(",", "."),
       },
       where: {
         id,
