74f7056426b9ac3a1d888b0904874cefe686165d
Verdant31 <joao.pedro3110@hotmail.com>
2023-01-09 19:29:16 -0400

    fixing format on prisma

--- src/pages/api/products/addProduct.ts
+++ src/pages/api/products/addProduct.ts
@@ -10,7 +10,7 @@ export default async function handler(
     const newProduct = await prisma.product.create({
       data: {
         name,
-        price: parseFloat(price.replace(",", ".")),
+        price: price.replace(",", "."),
       },
     });
     if (newProduct) {
